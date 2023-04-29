ef0fcbf1fa774064b3342c01b1e5332fdbcb010a
Shoko Sugito <fruitmachine0@gmail.com>
2023-04-29 18:49:30 +0900

    こうしん

--- components/organisms/note/index.vue
+++ components/organisms/note/index.vue
@@ -1,6 +1,14 @@
 <template lang="pug">
 div
-  div(v-html='content')
+  div(v-if='isEditing')
+    textarea(v-model='localContent')
+    button(@click='cancel') cancel
+    button(@click='save') save
+    h3 preview
+    div(v-html='markedContent')
+  template(v-else)
+    div(v-html='markedContent')
+    button(@click='isEditing = true') edit
 </template>
 
 <script>
@@ -12,11 +20,32 @@ export default {
   props: {
     item: { type: Object },
   },
+  data() {
+    return {
+      isEditing: false,
+      localContent: this.item.content.replace(/\\n/g, '\n'),
+    }
+  },
   computed: {
-    content() {
+    markedContent() {
       return DOMPurify.sanitize(
         marked.parse(this.item.content.replace(/\\n/g, '\n'))
       )
+    },
+  },
+  methods: {
+    cancel() {
+      this.localContent = this.item.content.replace(/\\n/g, '\n')
+      this.isEditing = false
+    },
+    save() {
+      this.$emit('item-updated', {
+        id: this.item.id,
+        updated: {
+          content: this.localContent,
+        },
+      })
+      this.isEditing = false
     },
   },
 }
--- components/templates/dailyNotes/index.vue
+++ components/templates/dailyNotes/index.vue
@@ -3,18 +3,28 @@ section
   div {{ dailyId }}
 
   ul(v-if='dailyNotes.length')
-    li(v-for='dailyNote in dailyNotes')
-      organisms-note(:item='dailyNote')
+    li(v-for='(dailyNote, index) in dailyNotes')
+      component(
+        :is='`organisms-${dailyNote.type}`',
+        :item='dailyNote',
+        @item-updated='updateDailyNote($event)'
+      )
 </template>
 
 <script>
-import { getDocs, collection } from 'firebase/firestore'
+import { getDocs, updateDoc, collection, doc } from 'firebase/firestore'
 import { db } from '~/plugins/firebase'
+
+const dateObject = new Date()
+const year = dateObject.getFullYear()
+const month = dateObject.getMonth() + 1
+const date = dateObject.getDate()
+const dailyId = `${year}${`0${month}`.slice(-2)}${`0${date}`.slice(-2)}`
 
 export default {
   name: 'TemplatesDailyNotes',
   props: {
-    dailyId: { type: String },
+    dailyId: { type: String, default: dailyId },
   },
   data() {
     return { dailyNotes: [] }
@@ -27,23 +37,32 @@ export default {
   watch: {
     uid(uid) {
       if (uid) {
-        this.fetchDailyNote()
+        this.fetchDailyNotes()
       }
     },
   },
   created() {
     if (this.uid) {
-      this.fetchDailyNote()
+      this.fetchDailyNotes()
     }
   },
   methods: {
-    async fetchDailyNote() {
+    async fetchDailyNotes() {
       const snapShots = await getDocs(
         collection(db, 'dailyNotes', this.uid, this.dailyId)
       )
+      this.dailyNotes = []
       snapShots.forEach((snapshot) => {
-        this.dailyNotes.push(snapshot.data())
+        this.dailyNotes.push({ ...snapshot.data(), id: snapshot.id })
       })
+    },
+
+    async updateDailyNote($event) {
+      await updateDoc(
+        doc(db, 'dailyNotes', this.uid, this.dailyId, $event.id),
+        $event.updated
+      )
+      this.fetchDailyNotes()
     },
   },
 }
