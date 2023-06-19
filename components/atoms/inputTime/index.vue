c4ddbb090546fa53616eec70fe28bf801f83d7c5
Shoko Sugito <fruitmachine0@gmail.com>
2023-06-20 07:17:11 +0900

    時間入力

---
+++ components/atoms/inputTime/index.vue
@@ -0,0 +1,131 @@
+<template lang="pug">
+.a-input-time
+  select(v-model='year', @input='onYearChanged($event.target.value)')
+    option(v-for='yearOption in yearOptions', :value='yearOption') {{ yearOption }}
+  | /
+  select(v-model='month', @input='onMonthChanged($event.target.value)')
+    option(v-for='monthOption in monthOptions', :value='monthOption') {{ monthOption }}
+  | /
+  select(v-model='date', @input='onDateChanged($event.target.value)')
+    option(v-for='dateOption in dateOptions', :value='dateOption') {{ dateOption }}
+
+  select(v-model='hours', @input='onHoursChanged($event.target.value)')
+    option(v-for='hoursOption in hoursOptions', :value='hoursOption') {{ hoursOption }}
+  | :
+  select(v-model='minutes', @input='onMinutesChanged($event.target.value)')
+    option(v-for='minutesOption in minutesOptions', :value='minutesOption') {{ minutesOption }}
+
+  button(@click='clear')
+    i.el-icon-error
+</template>
+
+  <script>
+export default {
+  name: 'AtomsInputTime',
+  props: {
+    value: { type: Date, default: '' },
+  },
+  data() {
+    return {
+      year: null,
+      month: null,
+      date: null,
+      hours: null,
+      minutes: null,
+    }
+  },
+  computed: {
+    yearOptions() {
+      const year = new Date().getFullYear()
+      return Array(10)
+        .fill(year)
+        .map((_, index) => year + index)
+    },
+    monthOptions() {
+      return Array(12)
+        .fill(1)
+        .map((month, index) => month + index)
+    },
+    dateOptions() {
+      const lastDate = new Date(this.year, this.month, 0).getDate()
+      return Array(lastDate)
+        .fill(1)
+        .map((date, index) => date + index)
+    },
+    hoursOptions() {
+      return Array(24)
+        .fill(0)
+        .map((hour, index) => hour + index)
+    },
+    minutesOptions() {
+      return Array(60)
+        .fill(0)
+        .map((minute, index) => `0${minute + index}`.slice(-2))
+    },
+  },
+  methods: {
+    clear() {
+      this.year = null
+      this.month = null
+      this.date = null
+      this.hours = null
+      this.minutes = null
+      this.$emit('input', null)
+    },
+    emitDate() {
+      const date = new Date(
+        this.year,
+        this.month - 1,
+        this.date,
+        this.hours,
+        this.minutes
+      )
+      this.$emit('input', date)
+    },
+    onYearChanged(year) {
+      this.year = year
+      this.month = this.month || 1
+      this.date = this.date || 1
+      this.emitDate()
+    },
+    onMonthChanged(month) {
+      this.year = this.year || this.yearOptions[0]
+      this.month = month
+      this.date = this.date || 1
+      this.emitDate()
+    },
+    onDateChanged(date) {
+      this.year = this.year || this.yearOptions[0]
+      this.month = this.month || 1
+      this.date = date
+      this.emitDate()
+    },
+    onHoursChanged(hour) {
+      this.year = this.year || this.yearOptions[0]
+      this.month = this.month || 1
+      this.date = this.date || 1
+      this.hours = hour
+      this.minutes = this.minutes || '00'
+      this.emitDate()
+    },
+    onMinutesChanged(minute) {
+      this.year = this.year || this.yearOptions[0]
+      this.month = this.month || 1
+      this.date = this.date || 1
+      this.hours = this.hours || 0
+      this.minutes = minute
+      this.emitDate()
+    },
+  },
+}
+</script>
+
+  <style lang="scss" scoped>
+@import '~/assets/stylesheets/input';
+.a-input-time {
+  @extend %input;
+  select {
+    appearance: none;
+  }
+}
+</style>
