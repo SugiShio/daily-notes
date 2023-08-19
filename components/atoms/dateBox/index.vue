220fbb987b298708520ae0c20a568d680802c432
Shoko Sugito <fruitmachine0@gmail.com>
2023-08-19 22:11:32 +0900

    refactor: 日付表示のコンポーネント

---
+++ components/atoms/dateBox/index.vue
@@ -0,0 +1,92 @@
+<template lang="pug">
+.a-date-box
+  time.a-date-box__main(:datetime='`${year}-${month}-${date}`')
+    span.a-date-box__year {{ year }}
+    span.a-date-box__date {{ month }}.{{ date }}
+      span.a-date-box__day {{ day }}
+
+  .a-date-box__selector
+    nuxt-link.a-date-box__prev(:to='linkPrevious')
+      i.el-icon-arrow-left
+    nuxt-link.a-date-box__next(:to='linkNext')
+      i.el-icon-arrow-right
+</template>
+
+<script>
+import {
+  convertDateIdToDate,
+  convertDateToDateId,
+  getDayText,
+} from '~/scripts/dateHelper'
+
+export default {
+  name: 'AtomsDateBox',
+  computed: {
+    dateObject() {
+      return convertDateIdToDate(this.$store.state.dailyId)
+    },
+    year() {
+      return this.dateObject.getFullYear()
+    },
+    month() {
+      return this.dateObject.getMonth() + 1
+    },
+    date() {
+      return this.dateObject.getDate()
+    },
+    day() {
+      return getDayText(this.dateObject)
+    },
+    linkNext() {
+      const nextDate = new Date(this.dateObject)
+      nextDate.setDate(this.dateObject.getDate() + 1)
+      const id = convertDateToDateId(nextDate)
+
+      return { name: 'id', params: { id } }
+    },
+    linkPrevious() {
+      const previousDate = new Date(this.dateObject)
+      previousDate.setDate(this.dateObject.getDate() - 1)
+      const id = convertDateToDateId(previousDate)
+
+      return { name: 'id', params: { id } }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/stylesheets/variables';
+
+.a-date-box {
+  &__main {
+    display: block;
+    font-family: $font-family-accent;
+  }
+
+  &__year {
+    display: block;
+  }
+
+  &__date {
+    display: block;
+    font-size: 24px;
+  }
+
+  &__day {
+    margin-left: 10px;
+  }
+
+  &__selector {
+    display: flex;
+    justify-content: space-between;
+    margin: 10px 0 30px;
+  }
+
+  &__prev,
+  &__next {
+    text-decoration: none;
+    font-size: 14px;
+  }
+}
+</style>
--- components/templates/dailyNotes/index.vue
+++ components/templates/dailyNotes/index.vue
@@ -1,17 +1,8 @@
 <template lang="pug">
 section.t-daily-notes
+  atoms-date-box
+
   organisms-task-list
-  time.t-daily-notes__title(:datetime='`${year}-${month}-${date}`')
-    span.t-daily-notes__year {{ year }}
-    span.t-daily-notes__date {{ month }}.{{ date }}
-      span.t-daily-notes__day {{ day }}
-
-  .t-daily-notes__selector
-    nuxt-link.t-daily-notes__prev(:to='linkPrevious')
-      i.el-icon-arrow-left
-    nuxt-link.t-daily-notes__next(:to='linkNext')
-      i.el-icon-arrow-right
-
   ul
     li.t-daily-notes__item(v-for='(dailyNote, id) in dailyNotes')
       component(
@@ -27,12 +18,6 @@ section.t-daily-notes
 </template>
 
 <script>
-import {
-  convertDateIdToDate,
-  convertDateToDateId,
-  getDayText,
-} from '~/scripts/dateHelper'
-
 export default {
   name: 'TemplatesDailyNotes',
   props: {
@@ -44,35 +29,6 @@ export default {
     },
     dailyNotes() {
       return this.$store.state.dailyNotes
-    },
-    dateObject() {
-      return convertDateIdToDate(this.$store.state.dailyId)
-    },
-    year() {
-      return this.dateObject.getFullYear()
-    },
-    month() {
-      return this.dateObject.getMonth() + 1
-    },
-    date() {
-      return this.dateObject.getDate()
-    },
-    day() {
-      return getDayText(this.dateObject)
-    },
-    linkNext() {
-      const nextDate = new Date(this.dateObject)
-      nextDate.setDate(this.dateObject.getDate() + 1)
-      const id = convertDateToDateId(nextDate)
-
-      return { name: 'id', params: { id } }
-    },
-    linkPrevious() {
-      const previousDate = new Date(this.dateObject)
-      previousDate.setDate(this.dateObject.getDate() - 1)
-      const id = convertDateToDateId(previousDate)
-
-      return { name: 'id', params: { id } }
     },
   },
   methods: {
@@ -97,37 +53,6 @@ export default {
 @import '~/assets/stylesheets/variables';
 
 .t-daily-notes {
-  &__title {
-    display: block;
-    font-family: $font-family-accent;
-    margin: 50px 0 10px;
-  }
-
-  &__year {
-    display: block;
-  }
-
-  &__date {
-    display: block;
-    font-size: 24px;
-  }
-
-  &__day {
-    margin-left: 10px;
-  }
-
-  &__selector {
-    display: flex;
-    justify-content: space-between;
-    margin: 10px 0 30px;
-  }
-
-  &__prev,
-  &__next {
-    text-decoration: none;
-    font-size: 14px;
-  }
-
   &__item {
     margin: 10px;
     background: rgba(#fff, 0.75);
