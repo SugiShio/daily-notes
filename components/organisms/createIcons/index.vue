735e33318437cb97e418c6e4252703489427c613
Shoko Sugito <fruitmachine0@gmail.com>
2023-08-23 21:20:15 +0900

    style

--- components/organisms/createIcons/index.vue
+++ components/organisms/createIcons/index.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 ul.o-create-icons
-  li.o-create-icons__icon(v-for='(t, index) in types')
-    button(@click='onCreateButtonClicked(index)')
-      i(:class='`el-icon-${t.icon}`')
+  li.o-create-icons__item(v-for='(t, index) in types')
+    button.o-create-icons__button(@click='onCreateButtonClicked(index)')
+      i.o-create-icons__icon(:class='`el-icon-${t.icon}`')
 </template>
 
 <script>
@@ -35,8 +35,27 @@ export default {
   margin: -4px;
   padding: 0 $padding-h;
 
+  &__item {
+    margin: 4px;
+  }
+
+  &__button {
+    border-radius: 20px;
+    width: 40px;
+    line-height: 40px;
+    transition: 0.3s;
+
+    &:hover {
+      background: rgba(#fff, 0.75);
+      box-shadow: 0 0 5px rgba($color-main-dark, 0.2);
+    }
+  }
+
   &__icon {
-    padding: 4px;
+    color: $color-main-dark;
+    font-size: 20px;
+    line-height: 1;
+    vertical-align: middle;
   }
 }
 </style>
