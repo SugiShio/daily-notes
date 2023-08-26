d1bdfd3c02c209c3f0ece904a9a9a89585aab1a7
Shoko Sugito <fruitmachine0@gmail.com>
2023-08-26 17:35:57 +0900

    pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    // https://pwa.nuxtjs.org/setup/
+    '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
